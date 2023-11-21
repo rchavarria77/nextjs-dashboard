d4ba111d1e25a662c090c27b69d85438a74e4389
Roberto Chavarria <rchavarria77@outlook.com>
2023-11-21 00:43:19 -0600

    updating node version

--- app/page.tsx
+++ app/page.tsx
@@ -42,8 +42,8 @@ export default function Page() {
             src='/hero-mobile.png'
             width={560}
             height={620}
-            className="block md:hidden"
-            alt="Screenshot of the dashboard project showing mobile version"
+            className='block md:hidden'
+            alt='Screenshot of the dashboard project showing mobile version'
           />
         </div>
       </div>
--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,4 +1,4 @@
-"use client"
+'use client';
 
 import {
   UserGroupIcon,
@@ -23,7 +23,7 @@ const links = [
 
 export default function NavLinks() {
   const pathname = usePathname();
-  
+
   return (
     <>
       {links.map((link) => {
@@ -36,11 +36,11 @@ export default function NavLinks() {
               'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
               {
                 'bg-sky-100 text-blue-600': pathname === link.href,
-              },
+              }
             )}
           >
-            <LinkIcon className="w-6" />
-            <p className="hidden md:block">{link.name}</p>
+            <LinkIcon className='w-6' />
+            <p className='hidden md:block'>{link.name}</p>
           </Link>
         );
       })}
